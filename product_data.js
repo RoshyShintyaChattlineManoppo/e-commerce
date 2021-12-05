82e434195a5d569ecf175b4bb0dea9b0d60356a2
RoshyShintyaChattlineManoppo <91876248+RoshyShintyaChattlineManoppo@users.noreply.github.com>
2021-12-05 17:57:39 +0700

    Create product_data.js

---
+++ product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "Wooden Chair",
+        description: "Kursi Model terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi biru.jpeg"
+    },
+    {
+        id: 2,
+        product_name: "Wooden Chair",
+        description: "Kursi Model terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi coklat.jpg"
+    },
+    {
+        id: 3,
+        product_name: "Wooden Chair",
+        description: "Kursi Model terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi merah muda.jpg"
+    },
+    {
+        id: 4,
+        product_name: "Wooden Chair",
+        description: "Kuri Model terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi biru.jpeg"
+    },
+    {
+        id: 5,
+        product_name: "Wooden Chair",
+        description: "Kursi Terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi coklat.jpg"
+    },
+    {
+        id: 6,
+        product_name: "Wooden Chair",
+        description: "Kursi Terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi merah muda.jpg"
+    },
+    {
+        id: 7,
+        product_name: "Wooden Chair",
+        description: "Kursi Terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi biru.jpeg"
+    },
+    {
+        id: 8,
+        product_name: "Wooden Chair",
+        description: "Kursi Terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi coklat.jpg"
+    },
+    {
+        id: 9,
+        product_name: "Wooden Chair",
+        description: "Kursi Terbaru dan Empuk",
+        price: 500.000,
+        currency: "Rp",
+        thumb: "./images/kursi merah muda.jpg"
+    }
+]
+export default product_card;
